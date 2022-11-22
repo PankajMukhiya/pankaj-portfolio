e0572ff85b1f781d84144e4734f50de74e2d3377
Pankaj <pankajx404@gmail.com>
2022-11-22 20:42:00 +0530

    resume update

--- frontend_react/src/assets/images.js
+++ frontend_react/src/assets/images.js
@@ -9,7 +9,7 @@ import react from "./Images/react.png";
 import redux from "./Images/redux.png";
 import sass from "./Images/sass.png";
 import about02 from "./Images/about02.png";
-import MyResumePdf from "./Images/Pankaj_Resume.pdf";
+import MyResumePdf from "./Images/Resume_Pankaj.pdf";
 
 // eslint-disable-next-line
 export default {
